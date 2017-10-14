2d024fca7d44977d8f18a7c57b163e9826b776ad
Jeremy Ruten <jeremy.ruten@gmail.com>
2017-10-14 12:08:38 -0600

    lapi.h ✓

--- lua-5.3.4-annotated/src/lapi.h
+++ lua-5.3.4-annotated/src/lapi.h
@@ -7,16 +7,30 @@
 #ifndef lapi_h
 #define lapi_h
 
+// This header defines some helper macros that are mainly used in lapi.c, but
+// also used in a handful of places elsewhere in the codebase.
 
 #include "llimits.h"
 #include "lstate.h"
 
+// Increments L->top, and asserts that this is never called when L->top already
+// points to the top of the stack for the current function. This is used
+// whenever pushing something on top of the stack.
 #define api_incr_top(L)   {L->top++; api_check(L, L->top <= L->ci->top, \
 				"stack overflow");}
 
+// Called when a function is returning. If multiple results are wanted from the
+// function call (as in the case of a tail call, e.g. `return f()`), then set
+// L->ci->top to L->top. (The return values start at the old stack frame's base
+// and proceed up to but not including L->top.) When is L->ci->top >= L->top
+// already?
 #define adjustresults(L,nres) \
     { if ((nres) == LUA_MULTRET && L->ci->top < L->top) L->ci->top = L->top; }
 
+// Used at the top of many Lua API functions, to make sure the number of
+// elements on the stack for the current frame is at least `n`. Lots of API
+// functions work with one or two values that are expected to be on top of the
+// stack when you call them.
 #define api_checknelems(L,n)	api_check(L, (n) < (L->top - L->ci->func), \
 				  "not enough elements in the stack")
 
